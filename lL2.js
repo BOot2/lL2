4b8a55e6f8a808bdb9bbedd6f4a07ae750b75902
BOot2 <43100466+BOot2@users.noreply.github.com>
2018-09-18 14:51:20 +0300

    Update lL2.js

--- lL2.js
+++ lL2.js
@@ -14,7 +14,7 @@ client.on('ready', () => {
   console.log('---------------')
 });
 
-const prefix = "-"
+const prefix = "ne"
 client.on('message', async msg => {
 	if (msg.author.bot) return undefined;
 	
@@ -315,7 +315,7 @@ client.on('message', function(message) {
 
 client.on('message', async message => {
             if(!message.channel.guild) return;
-             if (message.content.startsWith("$sets")) {
+             if (message.content.startsWith("ne")) {
 let args = message.content.split(' ').slice(1).join(' ');
             let sigMessage = await args;
             
